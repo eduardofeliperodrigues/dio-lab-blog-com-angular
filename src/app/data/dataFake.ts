4df3a89192779105b6304162da3fbdca11129201
Eduardo Rodrigues <contatoeduardofelipe@hotmail.com>
2023-09-29 01:43:32 -0300

    create a content page to read the articles

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    id: "1",
+    photo: 'https://media.istockphoto.com/id/825383494/photo/business-man-pushing-large-stone-up-to-hill-business-heavy-tasks-and-problems-concept.jpg?s=612x612&w=0&k=20&c=wtqvbQ6OIHitRVDPTtoT_1HKUAOgyqa7YzzTMXqGRaQ=',
+    title: 'Vencendo desafios',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, maxime natus, nulla dolore fugit impedit harum atque obcaecati'
+  },
+  {
+    id: "2",
+    photo: 'https://static.toiimg.com/photo/msid-53891743,width-96,height-65.cms',
+    title: 'Viagem a Paris',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, maxime natus, nulla dolore fugit impedit harum atque obcaecati'
+  },
+  {
+    id: "3",
+    photo: 'https://photographylife.com/wp-content/uploads/2017/01/Best-of-2016-Nasim-Mansurov-20.jpg',
+    title: 'Lagos Nordicos Lindos',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, maxime natus, nulla dolore fugit impedit harum atque obcaecati'
+  },
+  {
+    id: "4",
+    photo: 'https://cdn.pixabay.com/photo/2014/12/16/22/25/sunset-570881_1280.jpg',
+    title: 'Amizade é tudo na vida',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, maxime natus, nulla dolore fugit impedit harum atque obcaecati'
+  }
+]
